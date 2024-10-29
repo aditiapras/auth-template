50ac8035ffaeffcbe74d0bed5eb3914f7f523ede
aditiaprasdev <ip.hkpmt@gmail.com>
2024-10-29 17:15:14 +0700

    first commit

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,256 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+import { Check, Eye, EyeOff, Quote, X } from "lucide-react";
+import { useMemo, useState } from "react";
+
+export default function SignInPage() {
+  const [password, setPassword] = useState("");
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const toggleVisibility = () => setIsVisible((prevState) => !prevState);
+
+  const checkStrength = (pass: string) => {
+    const requirements = [
+      { regex: /.{8,}/, text: "At least 8 characters" },
+      { regex: /[0-9]/, text: "At least 1 number" },
+      { regex: /[a-z]/, text: "At least 1 lowercase letter" },
+      { regex: /[A-Z]/, text: "At least 1 uppercase letter" },
+    ];
+
+    return requirements.map((req) => ({
+      met: req.regex.test(pass),
+      text: req.text,
+    }));
+  };
+
+  const strength = checkStrength(password);
+
+  const strengthScore = useMemo(() => {
+    return strength.filter((req) => req.met).length;
+  }, [strength]);
+
+  const getStrengthColor = (score: number) => {
+    if (score === 0) return "bg-border";
+    if (score <= 1) return "bg-red-500";
+    if (score <= 2) return "bg-orange-500";
+    if (score === 3) return "bg-amber-500";
+    return "bg-emerald-500";
+  };
+
+  const getStrengthText = (score: number) => {
+    if (score === 0) return "Enter a password";
+    if (score <= 2) return "Weak password";
+    if (score === 3) return "Medium password";
+    return "Strong password";
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-50">
+      {/* <div className="w-full max-w-4xl flex overflow-hidden"> */}
+      <div className="hidden lg:block md:w-3/5 relative">
+        <div className="max-w-md mx-auto relative">
+          <Quote className="absolute -top-16 -left-5 text-zinc-500 size-10" />
+          {/* <p className="absolute -top-16 -left-5 text-zinc-500 text-8xl">"</p> */}
+          <p className="text-2xl font-semibold text-zinc-900 relative">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            tempore vel exercitationem consequuntur illo? Maxime quasi
+            voluptates praesentium quos facilis?
+          </p>
+        </div>
+        <div className="absolute rounded-full w-1/3 h-10 animate-pulse bg-teal-500/50 blur-3xl top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rotate-45"></div>
+        <div className="absolute rounded-full w-1/3 h-10 animate-pulse bg-emerald-500/50 blur-3xl top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 -rotate-45"></div>
+      </div>
+      <div className="w-full lg:w-2/5 p-10 flex shadow-lg border-l h-screen justify-center items-center border-zinc-300 bg-zinc-100">
+        <div className="w-full max-w-md mx-auto">
+          <div className="p-0 mb-4">
+            <p className="text-2xl font-bold">Sign up</p>
+            <p className="text-sm text-zinc-500 mt-2">
+              Create an account to get started
+            </p>
+          </div>
+
+          <div className="mt-8">
+            <Button
+              variant="outline"
+              className="w-full hover:bg-emerald-50 hover:border hover:border-emerald-500"
+            >
+              <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24">
+                <path
+                  d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                  fill="#4285F4"
+                />
+                <path
+                  d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                  fill="#34A853"
+                />
+                <path
+                  d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                  fill="#FBBC05"
+                />
+                <path
+                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                  fill="#EA4335"
+                />
+                <path d="M1 1h22v22H1z" fill="none" />
+              </svg>
+              Sign up with Google
+            </Button>
+          </div>
+          <div className="flex gap-4 w-full items-center my-5">
+            <Separator className="w-[45%] bg-zinc-300" />
+            <p className="text-sm text-zinc-500 w-[10%] text-center">or</p>
+            <Separator className="w-[45%] bg-zinc-300" />
+          </div>
+          <form>
+            <div className="flex flex-col gap-4">
+              <div className="space-y-2">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  placeholder="John Doe"
+                  type="text"
+                  required
+                  className="transition-all duration-300 focus-visible:ring-offset-0 focus-visible:ring-1 ring-offset-emerald-400 focus-visible:ring-emerald-400"
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="email">Email address</Label>
+                <Input
+                  id="email"
+                  placeholder="m@example.com"
+                  type="email"
+                  required
+                  className="transition-all duration-300 focus-visible:ring-offset-0 focus-visible:ring-1 ring-offset-emerald-400 focus-visible:ring-emerald-400"
+                />
+              </div>
+              <div className="">
+                {/* Password input field with toggle visibility button */}
+                <div className="space-y-2">
+                  <Label htmlFor="password">Password</Label>
+                  <div className="relative">
+                    <Input
+                      id="password"
+                      className="pe-9 transition-all duration-300 focus-visible:ring-offset-0 focus-visible:ring-1 ring-offset-emerald-400 focus-visible:ring-emerald-400"
+                      placeholder="Password"
+                      type={isVisible ? "text" : "password"}
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      aria-invalid={strengthScore < 4}
+                      aria-describedby="password-strength"
+                    />
+                    <button
+                      className="absolute inset-y-px end-px flex h-full w-9 items-center justify-center rounded-e-lg text-muted-foreground/80 transition-shadow hover:text-foreground focus-visible:border focus-visible:border-ring focus-visible:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/30 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+                      type="button"
+                      onClick={toggleVisibility}
+                      aria-label={isVisible ? "Hide password" : "Show password"}
+                      aria-pressed={isVisible}
+                      aria-controls="password"
+                    >
+                      {isVisible ? (
+                        <EyeOff
+                          className="text-emerald-500"
+                          size={16}
+                          strokeWidth={2}
+                          aria-hidden="true"
+                        />
+                      ) : (
+                        <Eye
+                          className="text-emerald-500"
+                          size={16}
+                          strokeWidth={2}
+                          aria-hidden="true"
+                        />
+                      )}
+                    </button>
+                  </div>
+                </div>
+                {password.length > 0 && (
+                  <>
+                    {/* Password strength indicator */}
+                    <div
+                      className="mb-4 mt-3 h-1 w-full overflow-hidden rounded-full bg-border"
+                      role="progressbar"
+                      aria-valuenow={strengthScore}
+                      aria-valuemin={0}
+                      aria-valuemax={4}
+                      aria-label="Password strength"
+                    >
+                      <div
+                        className={`h-full ${getStrengthColor(
+                          strengthScore
+                        )} transition-all duration-500 ease-out`}
+                        style={{ width: `${(strengthScore / 4) * 100}%` }}
+                      ></div>
+                    </div>
+
+                    {/* Password strength description */}
+                    <p
+                      id="password-strength"
+                      className="mb-2 text-sm font-medium text-foreground"
+                    >
+                      {getStrengthText(strengthScore)}. Must contain:
+                    </p>
+
+                    {/* Password requirements list */}
+                    <ul
+                      className="space-y-1.5"
+                      aria-label="Password requirements"
+                    >
+                      {strength.map((req, index) => (
+                        <li key={index} className="flex items-center space-x-2">
+                          {req.met ? (
+                            <Check
+                              size={16}
+                              className="text-emerald-500"
+                              aria-hidden="true"
+                            />
+                          ) : (
+                            <X
+                              size={16}
+                              className="text-muted-foreground/80"
+                              aria-hidden="true"
+                            />
+                          )}
+                          <span
+                            className={`text-xs ${
+                              req.met
+                                ? "text-emerald-500"
+                                : "text-muted-foreground"
+                            }`}
+                          >
+                            {req.text}
+                            <span className="sr-only">
+                              {req.met
+                                ? " - Requirement met"
+                                : " - Requirement not met"}
+                            </span>
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </div>
+              <Button className="w-full mt-5 bg-emerald-400 border border-emerald-500 hover:bg-emerald-300 text-emerald-900">
+                Sign up
+              </Button>
+            </div>
+          </form>
+          <div className="mt-5 text-center text-sm">
+            <p className="text-primary flex items-center justify-center gap-1">
+              Already have an account?
+              <Link href="/signin" className="underline hover:text-emerald-500">
+                Sign in here
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,105 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
-export default function Home() {
+export default function SignInPage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="min-h-screen flex items-center justify-center bg-zinc-50">
+      {/* <div className="w-full max-w-4xl flex overflow-hidden"> */}
+      <div className="hidden md:block md:w-3/5 relative">
+        <div className="max-w-md mx-auto">
+          <p className="text-2xl font-medium text-zinc-900 relative">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            tempore vel exercitationem consequuntur illo? Maxime quasi
+            voluptates praesentium quos facilis?
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="absolute rounded-full w-1/3 h-10 bg-teal-500/50 blur-3xl top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rotate-45"></div>
+        <div className="absolute rounded-full w-1/3 h-10 bg-emerald-500/50 blur-3xl top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 -rotate-45"></div>
+      </div>
+      <div className="w-full md:w-2/5 p-6 flex shadow-lg border-l h-screen justify-center items-center border-zinc-300 bg-zinc-100">
+        <div className="w-full max-w-md mx-auto">
+          <div className="p-0 mb-4">
+            <p className="text-2xl font-bold">Sign in</p>
+            <p className="text-sm text-zinc-500">
+              Enter your email to sign in to your account
+            </p>
+          </div>
+          <form>
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email">Email address</Label>
+                <Input
+                  id="email"
+                  placeholder="m@example.com"
+                  type="email"
+                  required
+                  className="transition-all duration-300 focus-visible:ring-offset-0 focus-visible:ring-1 ring-offset-emerald-400 focus-visible:ring-emerald-400"
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  className="transition-all duration-300 focus-visible:ring-offset-0 focus-visible:ring-1 ring-offset-emerald-400 focus-visible:ring-emerald-400"
+                />
+              </div>
+              <Button className="w-full bg-emerald-400 border border-emerald-500 hover:bg-emerald-300 text-emerald-900">
+                Sign In
+              </Button>
+            </div>
+          </form>
+          <div className="mt-4 text-left text-sm">
+            <Link
+              href="#"
+              className="text-primary hover:underline hover:text-emerald-500"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+          <div className="mt-2 text-left text-sm">
+            <Link
+              href="#"
+              className="text-primary hover:underline hover:text-emerald-500"
+            >
+              Don&apos;t have an account? Sign up
+            </Link>
+          </div>
+          <Separator className="my-4" />
+          <div className="space-y-4">
+            <Button
+              variant="outline"
+              className="w-full hover:bg-emerald-50 hover:border hover:border-emerald-500"
+            >
+              <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24">
+                <path
+                  d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                  fill="#4285F4"
+                />
+                <path
+                  d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                  fill="#34A853"
+                />
+                <path
+                  d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                  fill="#FBBC05"
+                />
+                <path
+                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                  fill="#EA4335"
+                />
+                <path d="M1 1h22v22H1z" fill="none" />
+              </svg>
+              Continue with Google
+            </Button>
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
     </div>
   );
 }
